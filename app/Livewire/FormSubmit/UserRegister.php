b090ab27dad8fa221894394299b1a5e3ae719ff3
muhammadayaan2001 <muhammad.ayan2977@gmail.com>
2023-10-28 07:06:34 -0700

    - validation or real time validation

--- app/Livewire/FormSubmit/UserRegister.php
+++ app/Livewire/FormSubmit/UserRegister.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\FormSubmit;
 
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class UserRegister extends Component
 {
 
-    public $name, $email, $password;
+    // in livewire version 3 you need to write this for real time validation
+    #[Rule('required|min:3')]
+    public $name;
+    #[Rule('required|email')]
+    public $email;
+    #[Rule('required|min:5|max:8')]
+    public $password;
 
     public function render()
     {
@@ -15,9 +22,29 @@ class UserRegister extends Component
     }
 
 
-    public function submit(){
+    /* if you to show or hide validate error when user type correct input, you have to create a function like this. 
+        The function is pre-defined called livewire lifecyle hook.
+        NOTE: If you are using livewire version below 3.
+        */
+    // public function update($propertyName)
+    // {
+    //     $this->validateOnly($propertyName, [
+    //         'name' => 'required',
+    //         'email' => 'required|email',
+    //         'password' => 'required|min:5|max:8'
+    //     ]);
+    // }
+
+    public function submit()
+    {
         // dd('ok');
+
+        // $this->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => 'required|min:5|max:8'
+        // ]);
+
         dd($this->name, $this->email, $this->password);
     }
-
 }
--- resources/views/livewire/form-submit/user-register.blade.php
+++ resources/views/livewire/form-submit/user-register.blade.php
@@ -4,13 +4,22 @@
 
     <form wire:submit.prevent="submit">
 
-        <input type="text" wire:model="name" placeholder="name">
+        <input type="text" wire:model.blur="name" placeholder="name">
+        @error('name')
+            <span style="color: red">{{ $message }}</span>
+        @enderror
         <br>
         <br>
-        <input type="email" wire:model="email" placeholder="email">
+        <input type="text" wire:model.blur="email" placeholder="email">
+        @error('email')
+            <span style="color: red">{{ $message }}</span>
+        @enderror
         <br>
         <br>
-        <input type="password" wire:model="password" placeholder="password">
+        <input type="password" wire:model.blur="password" placeholder="password">
+        @error('password')
+            <span style="color: red">{{ $message }}</span>
+        @enderror
         <br>
         <br>
         <button type="submit">Register</button>
